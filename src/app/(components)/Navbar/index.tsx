758893f5402e94c368081b90edeaf4bfb52ff676
IamAnthonyCruz <Charliecruz1224@gmail.com>
2025-08-25 09:51:34 -0400

    new commit

---
+++ src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Search, Settings } from "lucide-react";
+import Link from "next/link";
+const index = () => {
+  return (
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black dark:px-4 dark:py-3">
+      <div className="flex items-center gap-8">
+        <div className="relative flex h-min w-[200px]">
+          <Search className="tranform absolute top-1/2 left-[4px] mr-2 h-5 w-5 -translate-y-1/2 cursor-pointer dark:text-white" />
+          <input
+            className="b-2 w-full rounded border-none bg-gray-100 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+            type="search"
+            placeholder="Search"
+          />
+        </div>
+      </div>
+      <div className="flex items-center">
+        <Link
+          href="/settings"
+          className="h-min w-min rounded p-2 hover:bg-gray-100"
+        >
+          <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
+        </Link>
+        <div className="mr-5 ml-2 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
